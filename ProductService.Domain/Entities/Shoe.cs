9ebec10d91c87d08a6a7674ad8a96c2edb85b501
Khiem <phamxuankhiem1102@gmail.com>
2025-06-09 23:35:54 +0700

    Add Shoe class in Entities

---
+++ ProductService.Domain/Entities/Shoe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Domain.Entities
+{
+    public class Shoe
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public string Quantity { get; set; }    
+        public int Size { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+    }
+}
