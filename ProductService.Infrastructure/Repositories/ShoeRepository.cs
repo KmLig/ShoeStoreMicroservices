d0b7d2adf6becd2d416bf39b6ac80442fe06c3a4
Khiem <phamxuankhiem1102@gmail.com>
2025-06-10 22:58:32 +0700

    Implement ShoeRepository

---
+++ ProductService.Infrastructure/Repositories/ShoeRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Application.Interfaces;
+using ProductService.Domain.Entities;
+using ProductService.Infrastructure.Data;
+
+namespace ProductService.Infrastructure.Repositories
+{
+    public class ShoeRepository : IShoeRepository
+    {
+        private readonly AppDbContext _context;
+        public ShoeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Shoe>> GetAllAsync()
+        {
+            return await _context.Shoes.ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task<Shoe> GetByIdAsync(Guid id)
+        {
+            return await _context.Shoes.FindAsync(id).ConfigureAwait(false);
+        }
+
+        public async Task<Shoe> AddAsync(Shoe shoe)
+        {
+            _context.Shoes.Add(shoe);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+            return shoe;
+        }
+
+        public async Task UpdateAsync(Shoe shoe)
+        {
+            _context.Entry(shoe).State = EntityState.Modified;
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var shoe = await _context.Shoes.FindAsync(id).ConfigureAwait(false);
+            if (shoe != null)
+            {
+                _context.Shoes.Remove(shoe);
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
