6ca71d25bbb1e3f61dbd819bf213423453f9704e
Khiem <phamxuankhiem1102@gmail.com>
2025-06-10 22:29:54 +0700

    Add IShoeRepository interface in Application

---
+++ ProductService.Application/Interfaces/IShoeRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductService.Domain.Entities;
+
+namespace ProductService.Application.Interfaces
+{
+    public interface IShoeRepository
+    {
+        Task<IEnumerable<Shoe>> GetAllAsync();
+        Task<Shoe> GetByIdAsync(Guid id);
+        Task<Shoe> AddAsync(Shoe shoe);
+        Task UpdateAsync(Shoe shoe);
+        Task DeleteAsync(Guid id);
+    }
+}
