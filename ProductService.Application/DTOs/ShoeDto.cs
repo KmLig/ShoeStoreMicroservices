79d1c91d283710551514fc0134501bded860cf5e
Khiem <phamxuankhiem1102@gmail.com>
2025-06-11 22:22:25 +0700

    Add ShoeDTOs in ProductService.Application.DTOs

---
+++ ProductService.Application/DTOs/ShoeDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductService.Domain.DTOs
+{
+    public class ShoeDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Quantity { get; set; } = string.Empty;
+        public int Size { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
