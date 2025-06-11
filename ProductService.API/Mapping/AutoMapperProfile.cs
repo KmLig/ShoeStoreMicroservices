a4966d027ef1e7a366fdf9bf30de47350b29749b
Khiem <phamxuankhiem1102@gmail.com>
2025-06-11 23:04:55 +0700

    Add AutoMapperProfile

---
+++ ProductService.API/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductService.Application.DTOs;
+using ProductService.Domain.DTOs;
+using ProductService.Domain.Entities;
+
+namespace ProductService.API.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Shoe, ShoeDto>().ReverseMap();
+            CreateMap<CreateShoeDto, Shoe>();
+        }
+    }
+}
