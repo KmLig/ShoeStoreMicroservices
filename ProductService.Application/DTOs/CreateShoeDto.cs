38e8aed0fc5c0205b8e36380468dfe2946e82064
Khiem <phamxuankhiem1102@gmail.com>
2025-06-12 22:49:24 +0700

    Update CreateShoeDto

--- ProductService.Application/DTOs/CreateShoeDto.cs
+++ ProductService.Application/DTOs/CreateShoeDto.cs
@@ -12,5 +12,9 @@ namespace ProductService.Application.DTOs
         public string Brand { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+        public int Size { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
+}
 }
