168b09235cfd0d38ff2ae64f3cf19992319b50c4
Khiem <phamxuankhiem1102@gmail.com>
2025-06-12 22:36:15 +0700

    Add AutoMapper configuration in Program.cs

--- ProductService.API/Program.cs
+++ ProductService.API/Program.cs
@@ -1,7 +1,15 @@
+using ProductService.API.Mapping;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+// AutoMapper configuration
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+
+
+// Add Controlers 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
