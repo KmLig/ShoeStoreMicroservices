55dda52d5b609d403c49f23ca2d2548c2169cba9
Khiem <phamxuankhiem1102@gmail.com>
2025-06-14 15:44:53 +0700

    Add reference and create fields and constructor for ShoeServiceTests

--- ProductService.Tests/Services/ShoeServiceTests.cs
+++ ProductService.Tests/Services/ShoeServiceTests.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Moq;
+using ProductService.API.Mapping;
+using ProductService.Application.Interfaces;
+using ProductService.Application.Services;
 
 namespace ProductService.Tests.Services
 {
-    internal class ShoeServiceTests
+    public class ShoeServiceTests
     {
+        private readonly ShoeService _shoeService;
+        private readonly Mock<IShoeRepository> _repoMock;
+        private readonly IMapper _mapperMock;
+
+        public ShoeServiceTests()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new AutoMapperProfile()));
+            _mapperMock = config.CreateMapper();
+            _repoMock = new Mock<IShoeRepository>();
+            _shoeService = new ShoeService(_repoMock.Object, _mapperMock);
+        }
     }
 }
