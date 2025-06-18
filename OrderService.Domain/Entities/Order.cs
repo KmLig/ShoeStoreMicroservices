eec808b1d23e4939f31abdd9f4e35f6e09f1c9ee
Khiem <phamxuankhiem1102@gmail.com>
2025-06-18 21:38:59 +0700

    Create Order

---
+++ OrderService.Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderService.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public decimal TotalAmount => Items.Sum(item => item.Price * item.Quantity);
+
+    }
+}
