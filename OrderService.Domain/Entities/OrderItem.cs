3102b56c16857efae826e4d3f275c128bb67e7eb
Khiem <phamxuankhiem1102@gmail.com>
2025-06-18 21:37:44 +0700

    Create OrderItem

---
+++ OrderService.Domain/Entities/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderService.Domain.Entities
+{
+    public class OrderItem
+    {
+        public Guid ShoeId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
