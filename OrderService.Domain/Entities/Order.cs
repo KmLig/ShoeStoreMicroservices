de37c5a9b662afe3abe5bae49b52f09dad659749
Khiem <phamxuankhiem1102@gmail.com>
2025-06-24 21:45:27 +0700

    Update Order entity in Order domain

--- OrderService.Domain/Entities/Order.cs
+++ OrderService.Domain/Entities/Order.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; } = string.Empty;
         public DateTime OrderDate { get; set; }
         public List<OrderItem> Items { get; set; } = new List<OrderItem>();
         public decimal TotalAmount => Items.Sum(item => item.Price * item.Quantity);
