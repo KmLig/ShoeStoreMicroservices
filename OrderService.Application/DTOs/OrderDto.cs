eb94c2d1e05ad3758b359e3d192f80ab086a273d
Khiem <phamxuankhiem1102@gmail.com>
2025-06-24 21:44:58 +0700

    Add OrderDto in Order application

---
+++ OrderService.Application/DTOs/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderService.Application.DTOs
+{
+    internal class OrderDto
+    {
+        public Guid Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string CustomerEmail { get; set; } = string.Empty;
+        public List<OrderItemDto> Items { get; set; } = new List<OrderItemDto>();
+        public decimal TotalAmount => Items.Sum(item => item.Price * item.Quantity);
+    }
+}
