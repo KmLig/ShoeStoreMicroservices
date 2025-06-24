a468d5ab98e649adb981d7b4593953cde437ed7a
Khiem <phamxuankhiem1102@gmail.com>
2025-06-24 21:39:49 +0700

    Create IOrderService

---
+++ OrderService.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using OrderService.Domain.Entities;
+
+namespace OrderService.Application.Interfaces
+{
+    internal interface IOrderService
+    {
+        Task<Order?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Order>> GetAllAsync();
+        Task<Order> AddAsync(Order order);
+        Task UpdateAsync(Order order);
+        Task DeleteAsync(Guid id);
+    }
+}
