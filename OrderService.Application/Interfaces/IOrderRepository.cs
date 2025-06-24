27e0b8d994e9ffe77c8a69bb5c62cfb96126298c
Khiem <phamxuankhiem1102@gmail.com>
2025-06-24 21:28:38 +0700

    Remove unnecessary usings

--- OrderService.Application/Interfaces/IOrderRepository.cs
+++ OrderService.Application/Interfaces/IOrderRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OrderService.Domain.Entities;
+﻿using OrderService.Domain.Entities;
 
 namespace OrderService.Application.Interfaces
 {
