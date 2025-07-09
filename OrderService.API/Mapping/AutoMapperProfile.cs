ef89528c2cb81c916cff3bca68418273ceb58295
Khiem <phamxuankhiem1102@gmail.com>
2025-07-09 22:44:49 +0700

    Add AutoMapperProfile for OrderServiceAPI

---
+++ OrderService.API/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using OrderService.Application.DTOs;
+using OrderService.Domain.Entities;
+
+namespace OrderService.API.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<CreateOrderDto, Order>();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<CreateOrderItemDto, OrderItem>();
+        }
+    }
+}
