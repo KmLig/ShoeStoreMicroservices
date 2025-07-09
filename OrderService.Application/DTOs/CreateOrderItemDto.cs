f6aa6eee55abc6ca813769da8a4f56cb6ad7bfae
Khiem <phamxuankhiem1102@gmail.com>
2025-07-09 22:16:33 +0700

    Add CreateOrderItemDto

---
+++ OrderService.Application/DTOs/CreateOrderItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.DTOs
+{
+    public class CreateOrderItemDto
+    {
+        public Guid ShoeId { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }        
+    }
+}
